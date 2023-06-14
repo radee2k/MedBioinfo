bbdc948ae18eb1f2738f98bea43ebce7974f79e1
radee2k <radoslaw.grochowski@ki.se>
2023-06-14 17:06:13 +0200

    revise the analysis files and the on-site jupyter script up until blastn with DB

--- scripts/fastq_subsamples_blastn_vs_viral_refseq_sbatch_array.sh
+++ scripts/fastq_subsamples_blastn_vs_viral_refseq_sbatch_array.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1                   # nb of *tasks* to be run in // (usually 1), this task can be multithreaded (see cpus-per-task)
 #SBATCH --nodes=1                    # nb of nodes to reserve for each task (usually 1)
 #SBATCH --cpus-per-task=12            # nb of cpu (in fact cores) to reserve for each task /!\ job killed if commands below use more cores
-#SBATCH --mem=5GB                  # amount of RAM to reserve for the tasks /!\ job killed if commands below use more RAM
+#SBATCH --mem=32GB                  # amount of RAM to reserve for the tasks /!\ job killed if commands below use more RAM
 #SBATCH --time=02:00:00               # maximal wall clock duration (D-HH:MM) /!\ job killed if commands below take more time than reservation
 #SBATCH -o ./outputs_array/slurm.%A.%a.out   # standard output (STDOUT) redirected to these files (with Job ID and array ID in file names) HAS TO EXIST BEFORE BEING SUBMITTED!!!!!!!!!
 #SBATCH -e ./outputs_array/slurm.%A.%a.err   # standard error  (STDERR) redirected to these files (with Job ID and array ID in file names)
@@ -15,10 +15,9 @@
 #SBATCH --mail-user me@geemail.com
 
 #################################################################
-# Preparing work (cd to working dir, get hold of input data, convert/un-compress input data when needed etc.)
+# Preparing work env (cd to working dir, get hold of input data, convert/un-compress input data when needed etc.).
 workdir="/shared/ifbstor1/projects/2314_medbioinfo/radoslaw/MedBioinfo/analyses/blastn/array"
 datadir="/shared/ifbstor1/projects/2314_medbioinfo/radoslaw/MedBioinfo/data/merged_pairs"
-input_file1="/shared/ifbstor1/projects/2314_medbioinfo/radoslaw/MedBioinfo/data/merged_pairs/samples_for_blast/ERR6913112_sample_100.fna"
 accnum_file="/shared/ifbstor1/projects/2314_medbioinfo/radoslaw/MedBioinfo/analyses/rgrochowski_run_accessions.txt"
 output_file=${workdir}"analyses/blastn/ERR6913112_sample_blast"
 db="/shared/ifbstor1/projects/2314_medbioinfo/radoslaw/MedBioinfo/data/blast_db/refseq_viral_genomic"
@@ -39,38 +38,12 @@ input_file="${datadir}/${accnum}.flash.extendedFrags"
 echo ${accnum}
 echo ${input_file}
 
+# Convert fastq to fasta for blastn.
 srun seqkit fq2fa ${input_file}.fastq > ${input_file}.fasta
 
 output_file="${workdir}/${accnum}_blastn_vs_viral.out"
 
-# Start work
+# Start arrayed blastn.
 srun --job-name=${accnum} blastn -num_threads ${SLURM_CPUS_PER_TASK} -query ${input_file}.fasta -db ${db} -evalue 10 -outfmt 6 -perc_identity 70 -out ${output_file}
 
-
 echo END: `date`
-
-
-
-
-# this extracts the item number $SLURM_ARRAY_TASK_ID from the file of accnums
-# accnum=$(sed -n "$SLURM_ARRAY_TASK_ID"p ${accnum_file})
-# input_file="${datadir}/${accnum}.fastq"
-# alternatively, just extract the input file as the item number $SLURM_ARRAY_TASK_ID in the data dir listing
-# this alternative is less handy since we don't get hold of the isolated "accnum", which is very handy to name the srun step below :)
-# input_file=$(ls "${datadir}/*.fastq.gz" | sed -n ${SLURM_ARRAY_TASK_ID}p)
-
-# if the command below can't cope with compressed input
-# srun gunzip "${input_file}.gz"
-
-# because there are mutliple jobs running in // each output file needs to be made unique by post-fixing with $SLURM_ARRAY_TASK_ID and/or $accnum
-# output_file="${workdir}/ABCjob.${SLURM_ARRAY_TASK_ID}.${accnum}.out"
-
-#################################################################
-# Start work
-# srun --job-name=${accnum} some_abc_software --threads ${SLURM_CPUS_PER_TASK} --in ${input_file} --out ${output_file}
-
-#################################################################
-# Clean up (eg delete temp files, compress output, recompress input etc)
-# srun gzip ${input_file}
-#srun gzip ${output_file}
-
