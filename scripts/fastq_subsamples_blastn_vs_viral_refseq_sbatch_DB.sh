53b609795a937b35a753291b01ea7ca358a7053c
radee2k <radoslaw.grochowski@ki.se>
2023-06-15 21:56:39 +0200

    revised on_site script nearly until the end of blastn-nt_refseq benchmark

--- scripts/fastq_subsamples_blastn_vs_viral_refseq_sbatch_DB.sh
+++ scripts/fastq_subsamples_blastn_vs_viral_refseq_sbatch_DB.sh
@@ -3,9 +3,9 @@
 #SBATCH --partition=fast             # long, fast, etc.
 #SBATCH --ntasks=1                   # nb of *tasks* to be run in // (usually 1), this task can be multithreaded (see cpus-per-task)
 #SBATCH --nodes=1                    # nb of nodes to reserve for each task (usually 1)
-#SBATCH --cpus-per-task=1            # nb of cpu (in fact cores) to reserve for each task /!\ job killed if commands below use more cores
-#SBATCH --mem=70GB                  # amount of RAM to reserve for the tasks /!\ job killed if commands below use more RAM
-#SBATCH --time=02:00:00               # maximal wall clock duration (D-HH:MM) /!\ job killed if commands below take more time than reservation
+#SBATCH --cpus-per-task=10           # nb of cpu (in fact cores) to reserve for each task /!\ job killed if commands below use more cores
+#SBATCH --mem=50GB                  # amount of RAM to reserve for the tasks /!\ job killed if commands below use more RAM
+#SBATCH --time=01:00:00               # maximal wall clock duration (D-HH:MM) /!\ job killed if commands below take more time than reservation
 #SBATCH -o ./outputs_array_db/slurm.%A.%a.out   # standard output (STDOUT) redirected to these files (with Job ID and array ID in file names) HAS TO EXIST BEFORE BEING SUBMITTED!!!!!!!!!
 #SBATCH -e ./outputs_array_db/slurm.%A.%a.err   # standard error  (STDERR) redirected to these files (with Job ID and array ID in file names)
 # /!\ Note that the ./outputs/ dir above needs to exist in the dir where script is submitted **prior** to submitting this script
@@ -18,7 +18,6 @@
 # Preparing work (cd to working dir, get hold of input data, convert/un-compress input data when needed etc.)
 workdir="/shared/ifbstor1/projects/2314_medbioinfo/radoslaw/MedBioinfo/analyses/blastn/array_db"
 datadir="/shared/ifbstor1/projects/2314_medbioinfo/radoslaw/MedBioinfo/data/merged_pairs"
-input_file1="/shared/ifbstor1/projects/2314_medbioinfo/radoslaw/MedBioinfo/data/merged_pairs/samples_for_blast/ERR6913112_sample_100.fna"
 accnum_file="/shared/ifbstor1/projects/2314_medbioinfo/radoslaw/MedBioinfo/analyses/rgrochowski_run_accessions.txt"
 output_file=${workdir}"analyses/blastn/db/fastq_subsample_blastn_vs_NT"
 db="/shared/bank/nt/current/blast/nt"
@@ -39,14 +38,12 @@ input_file="${datadir}/${accnum}.flash.extendedFrags"
 echo ${accnum}
 echo ${input_file}
 
-
 seqkit range -r 1:10 ${input_file}.fasta > ${input_file}_10.fasta
-
 
 output_file="${workdir}/${accnum}_blastn_vs_viral.out"
 
 # Start work
-srun --job-name=${accnum} blastn -num_threads ${SLURM_CPUS_PER_TASK} -query ${input_file}_10.fasta -db ${db} -evalue 1E-10 -outfmt 6 -perc_identity 75 -out ${output_file}_10.out -max_target_seqs 5
+srun --job-name=${accnum} blastn -num_threads ${SLURM_CPUS_PER_TASK} -query ${input_file}_10.fasta -db ${db} -evalue 1E-6 -outfmt 6 -perc_identity 75 -out ${output_file}_10.out -max_target_seqs 5
 
 
 echo END: `date`
